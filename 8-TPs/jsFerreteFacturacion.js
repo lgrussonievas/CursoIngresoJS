d5db3bf61c4e44789710a2aed81045595929b038
Lucas Gabriel Russo Nievas <52494839+lgrussonievas@users.noreply.github.com>
2019-07-31 12:31:31 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,52 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+var p1;
+var p2;
+var p3;
+var suma;
+
+p1 = parseInt(document.getElementById("PrecioUno").value);
+p2 = parseInt(document.getElementById("PrecioDos").value);
+p3 = parseInt(document.getElementById("PrecioTres").value);
+
+suma = p1 + p2 + p3;
+
+alert("La suma es " + suma);
+
 }
 function Promedio () 
 {
-	
+var p1;
+var p2;
+var p3;
+var suma;
+var promedio;
+
+p1 = parseInt(document.getElementById("PrecioUno").value);
+p2 = parseInt(document.getElementById("PrecioDos").value);
+p3 = parseInt(document.getElementById("PrecioTres").value);
+
+suma = p1 + p2 + p3;
+promedio = suma / 3;
+
+alert("El promedio es " + promedio);
 }
+
 function PrecioFinal () 
 {
-	
+    var p1;
+    var p2;
+    var p3;
+    var suma;
+    var final;
+    
+    p1 = parseInt(document.getElementById("PrecioUno").value);
+    p2 = parseInt(document.getElementById("PrecioDos").value);
+    p3 = parseInt(document.getElementById("PrecioTres").value);
+    
+    suma = p1 + p2 + p3;
+    final = suma + suma * 0.21;
+    
+    alert("El precio final es " + final);	
 }
